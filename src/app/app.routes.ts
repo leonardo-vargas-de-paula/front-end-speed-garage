44b6ceaa47aaf4dc7857245932d2f2c2c01e887d
Leonardo Vargas <leonardovargasdpaula@gmail.com>
2025-04-14 16:17:20 -0400

    feat: adiciona rota para o login como root

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './tela-login-cadastro/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LoginComponent }
+];
+
