87acd1d477f94e62040bdd0360b850821c2f1767
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2025-04-22 21:41:29 -0400

    fix: corrige rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './tela-login-cadastro/login/login.component';
+import { HomeComponent } from './home/home.component';
+import { TelaLoginCadastroComponent } from './tela-login-cadastro/tela-login-cadastro.component';
+import { CadastroComponent } from './tela-login-cadastro/cadastro/cadastro.component';
 
 export const routes: Routes = [
-    { path: '', component: LoginComponent },
-    {path: 'register', component: LoginComponent}
+    { path: '', component: TelaLoginCadastroComponent },
+    {path: 'register', component: CadastroComponent},
+    {path: 'home', component: HomeComponent},
 ];
 
