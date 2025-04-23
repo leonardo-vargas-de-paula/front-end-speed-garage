fd80cce52950828df1e6d25ec80ec90b6de2063e
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2025-04-22 21:40:22 -0400

    feat: add header

---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SearchComponent } from "./search/search.component";
+
+@Component({
+  selector: 'app-header',
+  imports: [SearchComponent],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+}
