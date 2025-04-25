67483c9bda6ce75d8b5972c67b405f094756d544
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2025-04-25 18:48:01 -0400

    feat: adiciona profile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
-import { LoginComponent } from './tela-login-cadastro/login/login.component';
 import { HomeComponent } from './home/home.component';
 import { TelaLoginCadastroComponent } from './tela-login-cadastro/tela-login-cadastro.component';
 import { CadastroComponent } from './tela-login-cadastro/cadastro/cadastro.component';
+import { ProfileComponent } from './profile/profile.component';
 
 export const routes: Routes = [
     { path: '', component: TelaLoginCadastroComponent },
     {path: 'register', component: CadastroComponent},
     {path: 'home', component: HomeComponent},
+    {path: 'profile', component: ProfileComponent}
 ];
 
---
+++ src/app/profile/profile-main/profile-main.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-main',
+  imports: [],
+  templateUrl: './profile-main.component.html',
+  styleUrl: './profile-main.component.css'
+})
+export class ProfileMainComponent {
+
+}
