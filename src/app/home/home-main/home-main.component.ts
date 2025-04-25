57e03a37be267be706afbd4c9ddc69f6bf3de7eb
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2025-04-25 11:27:59 -0400

    feat: adiciona colunas da home

---
+++ src/app/home/home-main/home-main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-main',
+  imports: [],
+  templateUrl: './home-main.component.html',
+  styleUrl: './home-main.component.css'
+})
+export class HomeMainComponent {
+  mvw = "MOST VIWED OF THE WEEK";
+  topHundred = "TOP 100 OF ALL TIME";
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../shared/header/header.component";
+import { HomeMainComponent } from "./home-main/home-main.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, HomeMainComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
