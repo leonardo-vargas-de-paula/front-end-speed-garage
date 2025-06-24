9f1d7027f324293bb41e5e05ce3b57d00dc8d556
Leonardo Vargas <leonardovargasdpaula@gmail.com>
2025-06-24 10:15:36 -0400

    feat: adiciona delete

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -1,9 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../auth.service';
+import { ReviewService } from '../../review.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
@@ -17,10 +20,37 @@ export class CardComponent {
   @Input() marca: string | undefined = '';
   @Input() ano: number | undefined = 0;
 
-  constructor(private router: Router) {}
+  currentUserName: string = '';
+  @Output() reviewDeleted = new EventEmitter<number>();
+
+  constructor(private router: Router,
+    public authService: AuthService,
+    private reviewService: ReviewService
+  ) { }
+
+  ngOnInit() {
+    const user = this.authService.getCurrentUser();
+    this.currentUserName = user?.username || '';
+    console.log('Usuário logado:', this.currentUserName);
+    console.log('Usuário logado:', this.currentUserName);
+  }
 
   onReadMore() {
     this.router.navigate(['/review', this.id]);
+  }
+
+  onDelete(): void {
+    if (confirm('Tem certeza que deseja deletar esta review?')) {
+      this.reviewService.deleteReview(this.id).subscribe({
+        next: () => {
+          this.reviewDeleted.emit(this.id);
+        },
+        error: (err) => {
+          console.error('Erro ao deletar review:', err);
+          alert('Erro ao deletar review.');
+        }
+      });
+    }
   }
 
 }
